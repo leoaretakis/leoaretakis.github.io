2d0fc9dc7cc1fbe63b0954d8edea45d3f48bf924
Leonardo Aretakis <leonardoaretakis@leonardos-mbp.lan>
2024-09-08 22:18:44 +0100

    wip

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,71 @@
+// import Navbar from "@/components/navbar";
+// import { ThemeProvider } from "@/components/theme-provider";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { DATA } from "@/data/resume";
+// import { cn } from "@/lib/utils";
+// import type { Metadata } from "next";
+// import { Inter as FontSans } from "next/font/google";
+// import "./globals.css";
+
+// import { Theme } from "@radix-ui/themes";
+
+// const fontSans = FontSans({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL(DATA.url),
+//   title: {
+//     default: DATA.name,
+//     template: `%s | ${DATA.name}`,
+//   },
+//   description: DATA.description,
+//   openGraph: {
+//     title: `${DATA.name}`,
+//     description: DATA.description,
+//     url: DATA.url,
+//     siteName: `${DATA.name}`,
+//     locale: "en_US",
+//     type: "website",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+//   twitter: {
+//     title: `${DATA.name}`,
+//     card: "summary_large_image",
+//   },
+//   verification: {
+//     google: "",
+//     yandex: "",
+//   },
+// };
+
+export const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+    {/* // <Theme appearance="dark"> */}
+    {/* // <ThemeProvider attribute="class" defaultTheme="light">
+    //     <TooltipProvider delayDuration={0}> */}
+        {children}
+    {/* //     <Navbar />
+    //     </TooltipProvider>
+    // </ThemeProvider> */}
+    {/* // </Theme> */}
+    </>
+  );
+}
+
