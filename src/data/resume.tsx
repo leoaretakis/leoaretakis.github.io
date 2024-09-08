d7f113e40fbb0d52c2e864ffb80f069581ed99b7
Leonardo Aretakis <leonardoaretakis@leonardos-mbp.lan>
2024-09-09 00:51:21 +0100

    about section

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -3,15 +3,18 @@ import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
   name: "Leonardo Aretakis",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
+  initials: "LA",
+  url: "https://leoaretakis.github.io",
+  location: "Lisbon, Portugal",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
-  summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "/me.png",
+    "Software Engineer. Ruby on Rails and React/Javascript specialist. I love coding, solving problems and helping people.",
+  summary: [
+    "At the end of 2008, I started working with software development during my first year at the university. Since then I have worked in many business fields, from real state pricing software, industrial (metals & mining, oil & gas), education, and finance. Most of the time doing backend and frontend software development, with a few touches of data science and AI here and there.",
+    "Nowadays I am proficient in Software architecture, Coding (mostly Ruby on Rails, React, Javascript, and Java), and Leading Tech teams.",
+    "My goal is to help people and businesses grow and achieve their potential, using the tools and tech I feel I can help with."
+  ],
+  avatarUrl: "/foto_linkedin.jpeg",
   skills: [
     "React",
     "Next.js",
